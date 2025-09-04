cda027e455b35db74dd9faa439dff970ddd4df9f
José Edenilson Deras Calderón <josederas021@gmail.com>
2025-09-04 13:45:01 -0600

    Test 2

--- app.js
+++ app.js
@@ -2,10 +2,10 @@ import express from "express";
 
 const app = express();
 
-// /hola?nombre=Alguien&apellido=Apellido
+// /hola?param1=par1&param2=par2
 app.get("/hola", (req, res) => {
   const params = req.query;
-  res.send("Hola: " + params.nombre + " " + params.apellido);
+  res.json({ message: "Hola", param1: params.param1, param2: params.param2 });
 });
 
 const PORT = process.env.PORT || 3000;
