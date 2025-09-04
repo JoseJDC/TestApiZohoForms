b737e5356417fb7a21d4fe0e94d37b8dd54bfc64
José Edenilson Deras Calderón <josederas021@gmail.com>
2025-09-04 14:02:29 -0600

    Test 3

--- app.js
+++ app.js
@@ -5,17 +5,15 @@ const app = express();
 // /hola?param1=par1&param2=par2
 app.get("/hola", (req, res) => {
   const params = req.query;
-  res.json(
-    Array.of({
-      message: "Hola",
-      subObject: { nestedParam: "nestedValue" },
-      subObject2: { nestedParam2: "nestedValue2" },
-      subArray: [1, 2, 3, 4],
-      subArray2: ["a", "b", "c", "d"],
-      param1: params.param1,
-      param2: params.param2,
-    })
-  );
+  res.json({
+    message: "Hola",
+    subObject: { nestedParam: "nestedValue" },
+    subObject2: { nestedParam2: "nestedValue2" },
+    subArray: [1, 2, 3, 4],
+    subArray2: ["a", "b", "c", "d"],
+    param1: params.param1,
+    param2: params.param2,
+  });
 });
 
 const PORT = process.env.PORT || 3000;
