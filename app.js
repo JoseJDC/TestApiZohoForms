8e7893b79c00e7f71f543547df1a5c343977cb8b
José Edenilson Deras Calderón <josederas021@gmail.com>
2025-09-04 14:29:50 -0600

    Test 3

--- app.js
+++ app.js
@@ -5,12 +5,18 @@ const app = express();
 // /hola?param1=par1&param2=par2
 app.get("/hola", (req, res) => {
   const params = req.query;
+  const exampleObject = { nestedParam: "nestedValue" };
+  const exampleArray = [1, 2, 3, 4];
   res.json({
     message: "Hola",
-    subObject: { nestedParam: "nestedValue" },
-    subObject2: { nestedParam2: "nestedValue2" },
-    subArray: [1, 2, 3, 4],
-    subArray2: ["a", "b", "c", "d"],
+    subObject: exampleObject,
+    subObjectWithArray: { nestedParam: "nestedValue2", array: exampleArray },
+    subArray: exampleArray,
+    subArrayOfObjects: [
+      { obj1Param: "obj1Value" },
+      { obj2Param: "obj2Value" },
+      { obj3Param: "obj3Value" },
+    ],
     param1: params.param1,
     param2: params.param2,
   });
